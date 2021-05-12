1ea3bbf43cdb3b9cad30b99bc500cb651371d158
Alan <alan.kalbermatter.dev@gmail.com>
2021-05-11 21:10:05 -0300

    Jerarquia spring boot agregada

---
+++ src/main/java/com/utn/ds/Ds2021Application.java
@@ -0,0 +1,13 @@
+package com.utn.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ds2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ds2021Application.class, args);
+	}
+
+}
